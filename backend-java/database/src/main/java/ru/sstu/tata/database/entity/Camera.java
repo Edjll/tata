98d38aa926bf45ebc77dbc25cf86273f631fa237
Aleksei Dikun <aldi0221@netcracker.com>
2021-12-03 15:24:28 +0400

    cameras crud

---
+++ backend-java/database/src/main/java/ru/sstu/tata/database/entity/Camera.java
@@ -0,0 +1,39 @@
+package ru.sstu.tata.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Camera {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ip;
+
+    private String address;
+
+    private Double latitude; //ширина
+
+    private Double longitude; //долгота
+
+    private LocalDateTime startTime;
+
+
+    //время старта | date при регитсрации
+    //последний запуск | date обновляю после отправки в питон
+
+}
