231909e6aabcbf9e0d5b6803e599e7cac24285f6
Aleksei Dikun <aldi0221@netcracker.com>
2021-12-04 02:19:24 +0400

    mock camera data

--- backend-java/camera/src/main/java/ru/sstu/tata/service/CameraService.java
+++ backend-java/camera/src/main/java/ru/sstu/tata/service/CameraService.java
@@ -10,7 +10,8 @@ import ru.sstu.tata.database.repository.CameraRepository;
 import ru.sstu.tata.dto.CameraRequest;
 import ru.sstu.tata.dto.UpdateCameraRequest;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -51,5 +52,33 @@ public class CameraService {
                 .latitude(cameraRequest.getLatitude())
                 .longitude(cameraRequest.getLongitude())
                 .build();
+    }
+
+    //54.89705930266575 - min longitude
+    //59.960622914832285 - max longitude
+    //30.333209398741175 - min latitude
+    //52.296570598536796 - max latitude
+
+    //comment me if use real data
+    @PostConstruct
+    private void mockCameraData(){
+        double max_longitude = 59.960622914832285 - 54.89705930266575;
+        double min_longitude = 54.89705930266575;
+        double max_latitude = 52.296570598536796 - 30.333209398741175;
+        double min_latitude = 30.333209398741175;
+        for (int i = 0; i < 100; i ++) {
+            Camera camera = new Camera();
+            camera.setId((long) i);
+            camera.setIp("192.168.0.0"); //neuron-be нужен лишь id камеры
+            camera.setAddress("Саратов, ул. Московская");
+            double tmp_1 = max_longitude;
+            camera.setLongitude((Math.random() * ++tmp_1) + min_longitude);
+            double tmp_2 = max_latitude;
+            camera.setLatitude((Math.random() * ++tmp_2) + min_latitude);
+            camera.setStartTime(LocalDateTime.now().plusHours(i%12));
+            camera.setInterval(LocalDateTime.now());
+            cameraRepository.save(camera);
+        }
+
     }
 }
