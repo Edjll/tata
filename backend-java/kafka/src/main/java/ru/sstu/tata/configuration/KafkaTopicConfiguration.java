b149984407d6972486d8de34eb9c1b6e5f61d0b7
Aleksei Dikun <aldi0221@netcracker.com>
2021-12-03 17:36:12 +0400

    kafka

--- backend-java/kafka/src/main/java/ru/sstu/tata/configuration/KafkaTopicConfiguration.java
+++ backend-java/kafka/src/main/java/ru/sstu/tata/configuration/KafkaTopicConfiguration.java
@@ -1,19 +1,22 @@
-package ru.sstu.configuration;
+package ru.sstu.tata.configuration;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfiguration {
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
+
+    private String bootstrapAddress = "localhost:9092";
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
@@ -25,5 +28,13 @@ public class KafkaTopicConfiguration {
     @Bean
     public NewTopic topicCamera() {
         return new NewTopic("topic-camera", 1, (short) 1);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostConstruct
+    public void sendMessage() {
+        kafkaTemplate.send("topic-camera", "{\"id\":1,\"ip\":\"192.168.0.0\"}");
     }
 }
